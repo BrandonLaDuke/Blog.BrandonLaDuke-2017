41c863d808930b88e3c2620a6e24f0368e0d0f02
Brandon LaDuke <brandon@brandonladuke.com>
2017-07-02 02:52:19 -0400

    Added the ability to remove class onmouseleave to hide excerpt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function showExcerpt() {
   console.log("Hello from Excerpt");
-  document.getElementById('id')
+  document.getElementById('U1').classList.add("show-excerpt");
+}
+
+function hideExcerpt() {
+  console.log("Hello from RMExcerpt");
+  document.getElementById('U1').classList.remove("show-excerpt");
 }
